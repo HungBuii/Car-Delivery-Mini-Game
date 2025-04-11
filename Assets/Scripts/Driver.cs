9c768735409dfd9b6519a1b2f0333ea2d6b476b2
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-04-11 18:14:55 +0700

    add drive car, color package

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float steerSpeed = 60f;
+    [SerializeField] private float moveSpeed = 10f;
+
+    SpriteRenderer spriteRenderer;
+
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Steer the car based on player input
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+
+        // Move the car forward, backward
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Ouch!");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Package")
+        {
+            spriteRenderer.color = collision.GetComponent<SpriteRenderer>().color;
+            Debug.Log("Package " + spriteRenderer.color.ToString() + " collected!");
+            Destroy(collision.gameObject, 0.5f);
+        }
     }
 }
